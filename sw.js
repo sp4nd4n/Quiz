5b39dc0a97bfbf919c9d8f01d516db89cd10b9b2
SPANDAN CHATTERJEE <dserverformc@gmail.com>
2026-09-25 21:46:36 +0530

    Update cache version to punkd-shell-v2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'punkd-shell-v1';
+const CACHE = 'punkd-shell-v2';
 const SHELL = ['index.html', 'style.css', 'app.js', 'firebase-config.js', 'manifest.json', 'icon-192.png', 'icon-512.png'];
 
 self.addEventListener('install', (e) => {
